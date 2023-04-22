cceaa13c89e741cac605908c1a9627b27735754d
snailworks <15938712580@163.com>
2023-04-22 09:42:42 +0800

    asio_network

--- manager.h
+++ manager.h
@@ -15,18 +15,9 @@ namespace Manager
         class threadmanager
         {
             private:
-                // int val_=0;
-                // std::mutex mu_l;
-                // std::condition_variable producer,consume;
+
             public:
-                void AddSum(const int &val1, const int &val2);
-                void UseMutex(const std::string &message,int &input_val) ;//message:消息类型，input_val:输入的参数
-                void Producer(int i);
-                void Consumer();
-                std::string RecvDataSent(const std::string &senddata);
-                std::string RecvDataFrom(const std::string &recvdata);
-                std::string RecvData(const std::string &data);
-                int fun(int x);
+
                 
         };
 
